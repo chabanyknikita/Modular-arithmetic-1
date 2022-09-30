3633175bc4a707e794872297b55dffdae5a7ea90
Nikita Chabaniuk <92546152+chabanyknikita@users.noreply.github.com>
2022-09-30 20:51:26 +0300

    some changes

--- main.py
+++ main.py
@@ -48,13 +48,10 @@ class TheoryOfNumbers:
 
     def task_5(self, a, b):
         res = []
-
         for i in range(a, b + 1):
-
             for j in range(2, i):
                 if i % j == 0:
                     break
-
             else:
                 res.append(i)
         return random.choice(res)
